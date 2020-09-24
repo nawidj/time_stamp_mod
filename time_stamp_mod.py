7731300ce3e60a6161fb51e3f2c3862a809018f9
Nawid Jamali <njamali@honda-ri.com>
2020-09-23 22:53:57 -0700

    Add files via upload

---
+++ time_stamp_mod.py
@@ -0,0 +1,25 @@
+# Script to modify the date of a MOD file
+import struct
+import os
+import time
+import datetime
+import glob
+
+# Get list of files
+for file in glob.glob("*.MOI"):
+        print("Processing: {}".format(file))
+
+	fileName, _ = os.path.splitext(file)        
+	f = open(file, "rb")
+	b = f.read()
+	(year,) = struct.unpack(">H", b[6:8])
+	(month,) = struct.unpack("B", b[8])
+	(day,) = struct.unpack("B", b[9])
+	(hour,) = struct.unpack("B", b[10])
+	(minute,) = struct.unpack("B", b[11])
+	(second,) = struct.unpack(">H", b[12:14])
+
+	date = datetime.datetime(year=year, month=month, day=day, hour=hour, minute=minute, second=second/1000)
+	modTime = time.mktime(date.timetuple())
+
+	os.utime("{}.MOD".format(fileName), (modTime, modTime))
